017105f24123508fddc3fb04c2086f9626cd43ff
Yulia <edc1cde@rambler.ru>
2023-06-28 18:28:45 +0400

    add Task8

--- Seminar1/Task8/Program.cs
+++ Seminar1/Task8/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 8: Напишите программу, которая на вход принимает
+число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4; 8 -> 2, 4, 6, 8*/
+Console.WriteLine ("Введите число");
+int a = Int32.Parse(Console.ReadLine()); 
+int N = 2;
+while (N <= a)
+{
+    Console.WriteLine($"{N}");
+    N= N + 2;
+}
