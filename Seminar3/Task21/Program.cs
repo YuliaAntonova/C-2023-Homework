ac610ef249392b4f8b93d0a65b663804773fe60d
Yulia <edc1cde@rambler.ru>
2023-07-13 08:04:49 +0300

    add Tasks 19,21,23

---
+++ Seminar3/Task21/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит
+расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84; A (7,-5, 0); B (1,-1,9) -> 11.53 ()*/
+Random rand = new Random();
+int x1 = rand.Next(0, 10);
+int x2 = rand.Next(0, 10);
+int x3 = rand.Next(0, 10);
+int y1 = rand.Next(0, 10);
+int y2 = rand.Next(0, 10);
+int y3 = rand.Next(0, 10);
+Console.WriteLine($" A ({x1},{x2},{x3}); B ({y1},{y2},{y3}) ");
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(x3 - y3, 2)));
---
+++ Seminar3/Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125*/
+Random rand = new Random();
+int n = rand.Next(1,10);
+Console.WriteLine($"Выведем таблицу кубов цифры {n}");
+int counter = 1;
+while (true)
+{
+    Console.WriteLine($"{counter} -> {Math.Pow(counter, 3)}");
+    counter++;
+    if (counter > n) break;     
+}
