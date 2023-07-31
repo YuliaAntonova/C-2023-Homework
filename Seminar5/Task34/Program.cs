bd03dbf16bf18aa17fbb242e25128314290019ad
Yulia <edc1cde@rambler.ru>
2023-07-31 11:50:29 +0400

    add task 34

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 34: Задайте массив заполненный случайными 
+положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+int[] GetArray (int size, int min, int max) // метод заполнения
+{
+    int[] array = new int[size];// создание нового массива длиной size
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+int[] arrayFirst = GetArray(5, 100, 1000);
+Console.WriteLine(String.Join(", ", arrayFirst));
+int Calculate(int[] array)
+{
+    int n = 0;
+    foreach (int item in array)
+    {
+      if (item % 2 ==0)
+      n++;  
+    }
+    return n;
+}
+int sum = Calculate(arrayFirst);
+Console.WriteLine(sum);
