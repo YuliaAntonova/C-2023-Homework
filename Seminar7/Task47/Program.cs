915b725fcea5545d02610de01176aaceac8e91f7
Yulia <edc1cde@rambler.ru>
2023-08-02 17:02:54 +0400

    add Seminar7

---
+++ Seminar7/Task47/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 47. Задайте двумерный массив mxn, 
+заполненный случайными вещественными числами*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк массива: ");
+int row = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+double[,] arrayDouble = GetArray(row, columns);
+PrintArray(arrayDouble);
+double[,] GetArray (int n, int m)
+{
+    double[,] array = new double[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            array[i, j] = new Random().NextDouble() * m;
+        }
+    }
+    return array;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)// обращение к количеству строк
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) // обращение к количеству столбцов
+        {
+          Write($"{inArray[i,j]} ");  // вывод элементов двумерного массива с пробелом
+        }
+        WriteLine();// разделяем вывод на строки
+    }
+}
---
+++ Seminar7/ДоделатьTask52/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел.
+Найдите среднее арифметическое элементов в каждом столбце*/
+using System;
+using static System.Console;
+Clear();
+Write("Введите количество строк массива: ");
+int row = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(row, columns, 0, 9);
+PrintArray(array);
+WriteLine();
+AverageNumber(array);
+PrintArray(array);
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n]; 
+    for (int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+        {
+          Write($"{inArray[i,j]} ");  
+        }
+        WriteLine();
+    }
+}
+double[] AverageNumber(int[,] array)
+{
+    
+    double[] average = 0;
+    int sum = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+   
+    {
+        for (int i = 0; i <= array.GetLength(0); i++)
+        {
+            sum = array[i, j];
+            average = sum / i;
+        }
+        return sum;
+    }
+     
+}
+
